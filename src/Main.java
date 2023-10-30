1a4ed84251865dcd1041a4f518134e30a71333c3
dashoes15 <daltonasali@gmail.com>
2023-10-30 09:40:50 -0400

    lab 7 task 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        double celc;
+        boolean done = false;
+        double f;
+        String stop;
+        //°F = (9/5) °C+32
+        System.out.println("Enter a temperature in Celcius. Enter 'N' to stop.");
+        do {
+            if (scan.hasNextDouble()) {
+                celc = scan.nextDouble();
+                scan.nextLine();
+                    f = (1.8)*celc+32;
+                    System.out.println(f);
+            }else {
+                stop = scan.nextLine();
+                if (stop.equals("N")) {
+                    System.exit(0 );
+                }
+
+                System.out.println("You have entered something invalid. Try again.");
+            }
+        } while (!done);
     }
 }
